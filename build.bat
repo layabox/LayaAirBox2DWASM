2c6418c887c13c7d1233453a0c71e49224b12c89
jsjgameer <411963337@qq.com>
2024-01-05 21:05:56 +0800

    fix:fixed the out name config

--- build.bat
+++ build.bat
@@ -18,9 +18,9 @@ call emmake make
 
 echo Building wasm
 call emcc ../wasm/glue_stub.cpp bin/libbox2d.a  -I ../box2d/include -fno-rtti -fno-exceptions -s MODULARIZE=1 -s EXPORT_NAME=Box2D -s ALLOW_TABLE_GROWTH=1 --memory-init-file 0 -s FILESYSTEM=0 -s SUPPORT_LONGJMP=0 -s EXPORTED_FUNCTIONS=_malloc,_free -s ALLOW_MEMORY_GROWTH=1 -g3 -s ASSERTIONS=2 -s DEMANGLE_SUPPORT=1  --oformat=bare -o Box2D.bare.wasm
-call emcc --post-link Box2D.bare.wasm --post-js box2d_glue.js -fno-rtti -fno-exceptions -s MODULARIZE=1 -s EXPORT_NAME=Box2D -s ALLOW_TABLE_GROWTH=1 --memory-init-file 0 -s FILESYSTEM=0 -s SUPPORT_LONGJMP=0 -s EXPORTED_FUNCTIONS=_malloc,_free -s ALLOW_MEMORY_GROWTH=1 -g3 -s ASSERTIONS=2 -s DEMANGLE_SUPPORT=1 -o ../dist/Laya.Box2D.js
+call emcc --post-link Box2D.bare.wasm --post-js box2d_glue.js -fno-rtti -fno-exceptions -s MODULARIZE=1 -s EXPORT_NAME=Box2D -s ALLOW_TABLE_GROWTH=1 --memory-init-file 0 -s FILESYSTEM=0 -s SUPPORT_LONGJMP=0 -s EXPORTED_FUNCTIONS=_malloc,_free -s ALLOW_MEMORY_GROWTH=1 -g3 -s ASSERTIONS=2 -s DEMANGLE_SUPPORT=1 -o ../dist/laya.Box2D.wasm.js
 
 echo Building js
 call emcc ../wasm/glue_stub.cpp bin/libbox2d.a  -I ../box2d/include -fno-rtti -fno-exceptions -s MODULARIZE=1 -s EXPORT_NAME=Box2D -s ALLOW_TABLE_GROWTH=1 --memory-init-file 0 -s FILESYSTEM=0 -s SUPPORT_LONGJMP=0 -s EXPORTED_FUNCTIONS=_malloc,_free -s ALLOW_MEMORY_GROWTH=1 -g3 -s ASSERTIONS=2 -s DEMANGLE_SUPPORT=1 -s WASM=0 --oformat=bare -o Box2D.bare.wasm
-call emcc --post-link Box2D.bare.wasm --post-js box2d_glue.js -fno-rtti -fno-exceptions -s MODULARIZE=1 -s EXPORT_NAME=Box2D -s ALLOW_TABLE_GROWTH=1 --memory-init-file 0 -s FILESYSTEM=0 -s SUPPORT_LONGJMP=0 -s EXPORTED_FUNCTIONS=_malloc,_free -s ALLOW_MEMORY_GROWTH=1 -g3 -s ASSERTIONS=2 -s DEMANGLE_SUPPORT=1 -s WASM=0 -o ../dist/Laya.Box2D.wasm.js
+call emcc --post-link Box2D.bare.wasm --post-js box2d_glue.js -fno-rtti -fno-exceptions -s MODULARIZE=1 -s EXPORT_NAME=Box2D -s ALLOW_TABLE_GROWTH=1 --memory-init-file 0 -s FILESYSTEM=0 -s SUPPORT_LONGJMP=0 -s EXPORTED_FUNCTIONS=_malloc,_free -s ALLOW_MEMORY_GROWTH=1 -g3 -s ASSERTIONS=2 -s DEMANGLE_SUPPORT=1 -s WASM=0 -o ../dist/laya.Box2D.js
 cd ..
